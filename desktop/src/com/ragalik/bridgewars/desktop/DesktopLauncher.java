503c7c99bdefd90072c7fbf5d2f5d4f42c83d438
GitlHubber <Drankevich.vitolik@mail.ru>
2019-01-31 15:38:25 +0300

    Initial commit

---
+++ desktop/src/com/ragalik/bridgewars/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ragalik.bridgewars.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ragalik.bridgewars.BridgeWars;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BridgeWars(), config);
+	}
+}
